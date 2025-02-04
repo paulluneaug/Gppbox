900d0907ca94602bbf2e70591f0fe9e9a55a1af3
p.luneau <paul.luneau@mail.com>
2025-02-05 00:40:04 +0100

    Simple enemy behaviour

--- Gppbox/Enemy.cpp
+++ Gppbox/Enemy.cpp
@@ -1,5 +1,28 @@
 #include "Enemy.h"
+#include <cstdlib>
+
 
 EnemyEntity::EnemyEntity(Game& r_game) : Entity(r_game, { 1, 1 })
 {
+	m_goingRight = std::rand() > (RAND_MAX / 2);
+}
+
+void EnemyEntity::UpdatePosition(float deltaTime)
+{
+	UpdateDirection(deltaTime);
+	Entity::UpdatePosition(deltaTime);
+}
+
+void EnemyEntity::UpdateDirection(float deltaTime)
+{
+	if (m_goingRight && CollidesRight(deltaTime))
+	{
+		m_goingRight = false;
+	}
+	else if (!m_goingRight && CollidesLeft(deltaTime)) 
+	{
+		m_goingRight = true;
+	}
+
+	Input.x = m_goingRight ? 1 : -1;
 }
--- Gppbox/Enemy.h
+++ Gppbox/Enemy.h
@@ -4,7 +4,16 @@
 
 class EnemyEntity : public Entity
 {
+private :
+	bool m_goingRight;
+
 public:
 	EnemyEntity(Game& r_game);
+
+protected:
+	void UpdatePosition(float deltaTime) override;
+
+private:
+	void UpdateDirection(float deltaTime);
 };
 
