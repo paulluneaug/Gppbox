aaa9b008e89d947a00f822964c4f704098085e72
p.luneau <paul.luneau@mail.com>
2025-02-09 18:59:04 +0100

    Added a Timer class

---
+++ Gppbox/Timer.cpp
@@ -0,0 +1,76 @@
+#include "Timer.h"
+
+#include <algorithm>
+
+Timer::Timer(float duration, bool repeat, float startTime) :
+	m_duration(std::max(0.0f, duration)),
+	m_repeat(repeat),
+	m_elapsedTime(startTime)
+{
+}
+
+bool Timer::IsRunning() const
+{
+	return m_isRunning;
+}
+
+float Timer::GetDuration() const
+{
+	return m_duration;
+}
+
+void Timer::SetDuration(float duration)
+{
+	m_duration = duration;
+	Reset();
+}
+
+float Timer::GetProgress() const
+{
+	return m_elapsedTime / m_duration;
+}
+
+void Timer::Start()
+{
+	Pause(false);
+}
+
+void Timer::Stop()
+{
+	Pause(true);
+	Reset();
+}
+
+void Timer::Pause(bool pause)
+{
+	m_isRunning = !pause;
+}
+
+void Timer::Reset()
+{
+	m_elapsedTime = 0.0f;
+}
+
+bool Timer::Update(float deltaTime)
+{
+	if (!m_isRunning)
+	{
+		return false;
+	}
+
+	m_elapsedTime += deltaTime;
+	if (m_elapsedTime < m_duration)
+	{
+		return false;
+	}
+
+	if (m_repeat)
+	{
+		m_elapsedTime -= m_duration;
+	}
+	else
+	{
+		Stop();
+	}
+	return true;
+}
---
+++ Gppbox/Timer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class Timer
+{
+private :
+
+	float m_duration = 1.0f;
+	bool m_repeat = false;
+
+
+	float m_elapsedTime = 0.0f;
+	bool m_isRunning = false;
+
+public:
+	Timer(float duration, bool repeat, float startTime = 0.0f);
+
+	// Getters - Setters
+	bool IsRunning() const;
+
+	float GetDuration() const;
+	void SetDuration(float duration);
+
+	float GetProgress() const;
+
+	void Start();
+	void Stop();
+	void Pause(bool pause);
+	void Reset();
+
+	bool Update(float deltaTime);
+};
