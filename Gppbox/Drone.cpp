3a73fef24cd6116f7df74d28c6fcf62f5fc1f78e
p.luneau <paul.luneau@mail.com>
2025-02-16 18:38:43 +0100

    Simple Drone behaviour (far from perfect for now)

---
+++ Gppbox/Drone.cpp
@@ -0,0 +1,85 @@
+#include "Drone.h"
+#include "Entity.h"
+#include "Utils.h"
+
+Drone::Drone(Game& r_game, float reloadTime, Vector2f startPosition) :
+	Super(r_game, reloadTime),
+	m_position(startPosition)
+{
+	float spriteSize = 0.5 * Consts::GRID_SIZE;
+	m_sprite = new RectangleShape({ spriteSize , spriteSize });
+	m_sprite->setOrigin({ spriteSize / 2 , spriteSize / 2 });
+}
+
+Drone::~Drone()
+{
+	delete m_sprite;
+}
+
+void Drone::Update(float deltaTime, float posX, float posY, float dirX, float dirY)
+{
+	Vector2f toPLayer = { posX - m_position.x, posY - m_position.y };
+	Vector2f targetPosition = Vector2f{ posX, posY } - Utils::Normalize(toPLayer) * TARGET_DISTANCE_FROM_PLAYER;
+
+	m_position = Utils::SmoothLerp(m_position, targetPosition, deltaTime, LERP_HALF_TIME);
+
+	if (m_reloadTimer.Update(deltaTime))
+	{
+		Shoot();
+	}
+
+	// Only deletes one projectile per frame
+	int projectileToDeleteIndex = -1;
+	for (int iProj = 0; iProj < m_shotProjectiles.size(); ++iProj)
+	{
+		Projectile* projectile = m_shotProjectiles[iProj];
+		if (projectile->IsAlive)
+		{
+			projectile->Update(deltaTime);
+		}
+		else
+		{
+			projectileToDeleteIndex = iProj;
+		}
+	}
+
+	if (projectileToDeleteIndex != -1)
+	{
+		Projectile* projectile = m_shotProjectiles[projectileToDeleteIndex];
+		m_shotProjectiles.erase(m_shotProjectiles.begin() + projectileToDeleteIndex);
+		delete projectile;
+	}
+}
+
+void Drone::Draw(sf::RenderWindow& r_window)
+{
+	m_sprite->setPosition(m_position);
+	r_window.draw(*m_sprite);
+
+	for (Projectile* projectile : m_shotProjectiles)
+	{
+		if (projectile->IsAlive)
+		{
+			projectile->Draw(r_window);
+		}
+	}
+}
+
+void Drone::Shoot()
+{
+	Entity* target = m_game.GetClosestEnemy(m_position.x, m_position.y);
+	if (target == nullptr || !target->IsAlive())
+	{
+		return;
+	}
+
+	Vector2f toTarget = { target->Xx - m_position.x, -(target->Yy - m_position.y) };
+
+	if (Utils::SqrMagnitude(toTarget) > MAX_RANGE * MAX_RANGE) 
+	{
+		return;
+	}
+
+	Projectile* newProjectile = new Projectile(m_game, m_position, Utils::Normalize(toTarget));
+	m_shotProjectiles.push_back(newProjectile);
+}
---
+++ Gppbox/Drone.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Weapon.h"
+#include "Projectile.h"
+#include "C.hpp"
+
+class Drone : public Weapon
+{
+	typedef Weapon Super;
+
+private:
+	static constexpr float MAX_RANGE = 20.0f * Consts::GRID_SIZE;
+	static constexpr float TARGET_DISTANCE_FROM_PLAYER = 4.0f * Consts::GRID_SIZE;
+	static constexpr float LERP_HALF_TIME = 1.0f;
+
+private:
+	sf::Shape* m_sprite;
+
+	Vector2f m_position;
+	std::vector<Projectile*> m_shotProjectiles;
+
+public:
+	Drone(Game& r_game, float reloadTime, Vector2f startPosition);
+	virtual ~Drone();
+
+	virtual void Update(float deltaTime, float posX, float posY, float dirX, float dirY) override;
+	virtual void Draw(sf::RenderWindow& r_window) override;
+
+private:
+	void Shoot();
+};
+
